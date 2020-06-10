e4c7ef0c916bcfb74c4a7b04ac180a27db2c19fa
jackjasper <jwjasper@umich.edu>
2020-06-10 11:54:57 -0500

    6 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,10 +8,15 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class LoginComponent {
   default = "Enter your access key";
+  pwd = "123";
 
   constructor(private router: Router) { }
 
   load() {
-    this.router.navigate(['/success']);
+    var key = (<HTMLInputElement>document.getElementById("login-input")).value
+    console.log(key)
+    if (key == this.pwd) {
+      this.router.navigate(['/success']);
+    }
   }
 }
