b95e13822b7ce18c3c3a29da03ccdd1df8590980
jackjasper <jwjasper@umich.edu>
2020-06-10 12:49:08 -0500

    6 files added

---
+++ src/app/success/resume/resume.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'resume',
+  templateUrl: './resume.component.html',
+  styleUrls: [ './resume.component.css' ]
+})
+export class AppComponent  {
+  src = "https://jackjasper.github.io/assets/HireJackJasper/2020-06-08 15-51.pdf";
+}
